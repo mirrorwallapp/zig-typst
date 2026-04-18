4f6dfc4b08c6ace3331fc37262f377952c6f61f0
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2026-04-17 19:42:35 -0500

    add lexer

---
+++ src/lexer.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+
+const mem = std.mem;
+const testing = std.testing;
+const expect = testing.expect;
+const pageAllocator = std.heap.page_allocator;
+
+pub const Token = struct {
+    tag: Tag,
+    loc: Loc,
+
+    pub const Loc = struct {
+        start: usize,
+        end: usize,
+    };
+
+    pub const Tag = enum {
+        eof,
+        err,
+        shebang,
+        line_comment,
+        block_comment,
+        markup,
+        text,
+        space,
+        linebreak,
+        paragrah_break,
+        escape,
+        shorthand,
+        smartquote,
+        strong,
+        empasized,
+        raw,
+        raw_lang,
+        raw_delim,
+        raw_trimmed,
+        link,
+        label,
+        ref,
+        ref_marker,
+        heading,
+        heading_marker,
+        list_item,
+        list_marker,
+        enum_item,
+        enum_marker,
+        term_item,
+        term_marker,
+        eq,
+
+        math,
+        math_text,
+        math_ident,
+        math_shorthand,
+        math_alignpoint,
+        math_call,
+        math_args,
+        math_delim,
+        math_attach,
+        math_primes,
+        math_frac,
+        math_root,
+
+        hash,
+        l_brace,
+        r_brace,
+    };
+};
